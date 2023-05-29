185e897d371a06db2a4ed97067223c1b47258ee9
tanvimagdum <magdum.t@northeastern.edu>
2023-05-29 03:36:01 -0400

    New Meetups Page

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_DATA = [
@@ -19,10 +20,42 @@ const DUMMY_DATA = [
 
 function AllMeetupsPage() {
 
+    const[isLoading, setIsLoading] = useState(true);
+    const[loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://react-meetup-60384-default-rtdb.firebaseio.com/meetups.json')
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            const meetups = [];
+            for(const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        });
+    }, []);
+
+    if(isLoading) {
+        return(
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
     return(
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA}/>
+            <MeetupList meetups={loadedMeetups}/>
             
         </section>
     );
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
+
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-meetup-60384-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            navigate('/');
+        });
+    }
 
     return(
         <div>
-            NewMeetupPage
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </div>
     );
 
